5afd14a73680f0afde75ae3a3c7843376a845e3a
edenko <goodeden3@gmail.com>
2023-09-07 15:31:42 +0900

    firefox test 2

--- src/main/java/com/example/medium/Controller/index.java
+++ src/main/java/com/example/medium/Controller/index.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,8 @@ public class index {
 
         FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--headless");
-        options.setLogLevel(FirefoxDriverLogLevel.TRACE);
+        options.addArguments("--disable-gpu");
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
         System.setProperty("webdriver.gecko.driver", geckoDriverPath);
         WebDriver driver = new FirefoxDriver(options);
         driver.get("https://medium.com/illumination/sonar-for-soil-how-we-can-use-sound-to-save-our-farms-and-forests-8521ebf86bb8");
